5a8e6337c0548aa3b51d6cfc408a9406377bf64d
Deflandrenassim <nassim.def@gmail.com>
2021-10-07 11:40:48 +0200

    update fixtures

--- src/DataFixtures/MessagesFixtures.php
+++ src/DataFixtures/MessagesFixtures.php
@@ -1,27 +1,47 @@
 <?php
 
-namespace Controller\DataFixtures\MessagesFixtures;
-use App\Entity\Messages;
-use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Persistence\ObjectManager;
+namespace App\DataFixtures;
 
-class Message extends Fixture
+use App\Entity\Messages;
+use App\Entity\Users;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class MessagesFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
+        // utilise le faker pour envoyer des donnés aléatoires
         $faker = Factory::create('fr_FR');
 
-        for ($nbMessages= 1; $nbMessages <50; $nbMessages++){
+        // Créer 30 messages avec une boucle for 
+        for ($nbMessages = 1; $nbMessages <20; $nbMessages++) {
 
             $product = new Messages();
-            $product->SetObject($faker->sentence(30));
-            $product->SetStatus($faker->text);
+            $product->SetObject($faker->sentence(10));
+            $product->setMessage($faker->text(50));
+            $product->SetStatus($faker->boolean(50)); // mettre un chiffre 50 pour avoir 50% de chance d'avoir TRUE
+
+
+
             $product->setCreatedAt(\DateTimeImmutable::createFromMutable($faker->dateTimeBetween('now', '+30 years')));
+            $product->SetUser($this->getReference("UserId")); // récuperer l'objet dans UsersFixture de la table UsersId
+
+            $product->setTarget($this->getReference("TargetId")); // récuperer l'objet dans UsersFixture de la table TargetId
+            $manager->persist($product);
         }
 
 
-
-        $manager->persist($product);
+        // persistes les messages
+        // envoi la bdd 
         $manager->flush();
+    }
+    public function getDependencies()
+    {
+        return [
+            UsersFixtures::class,
+        ];
     }
 }
--- src/DataFixtures/UsersFixtures.php
+++ src/DataFixtures/UsersFixtures.php
@@ -59,5 +59,8 @@ class UsersFixtures extends Fixture
 
 
         $manager->flush();
+
+        $this->addReference("UserId", $Users);
+        $this->addReference("TargetId", $Users);
     }
 }
