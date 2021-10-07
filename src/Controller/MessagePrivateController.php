afe454cede58519b17e140d7785ff6471ebdb864
Deflandrenassim <nassim.def@gmail.com>
2021-10-07 14:43:01 +0200

    update bdd

--- src/Controller/MessagePrivateController.php
+++ src/Controller/MessagePrivateController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Users;
+use App\Repository\UsersRepository;
 use App\Entity\Messages;
 use App\Form\MessageType;
+use DateTime;
+use Doctrine\ORM\Mapping\Id;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,10 +18,12 @@ class MessagePrivateController extends AbstractController
     /**
      * @Route("/messageprivate", name="message_private")
      */
-    public function index(Request $request): Response
+    public function index(Request $request, UsersRepository $repository): Response
     {
             // Je prépare l'entité 
         $messages = new Messages();
+
+        $target = $repository->findAll();
         
 
         // je crée un formulaire symfony 
@@ -28,13 +34,19 @@ class MessagePrivateController extends AbstractController
         // si le formulaire a été soumis 
 
         if($form->isSubmitted()){
+            
+            $user =$this->getUser();
+            $messages->SetUser($user);
+            $messages->setCreatedAt(new \DateTime());
+            $messages->setStatus(0);
 
             // on enregistre le champ en bdd 
             $em = $this->getDoctrine()->getManager();
+            
             $em->persist($messages);
             $em->flush(); // Insert en bdd
             
-
+          
 
             
            // Permet d'etre redigere dans la meme page avec la validation du message envoyé 
@@ -42,7 +54,8 @@ class MessagePrivateController extends AbstractController
         }
         
         return $this->render('message_private/index.html.twig', [
-            'controller_name' => 'MessagePrivateController'
+            'MessageForm' => $form->createView(),
+
 
            
         ]);
